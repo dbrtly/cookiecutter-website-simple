d158e95736e2007059df41cdaa822ed9a242e6ef
Daniel Bartley <dbrtly00@gmail.com>
2025-06-13 22:17:41 +1000

    add post hook

---
+++ foo/hooks/post_gen_project.py
@@ -0,0 +1,8 @@
+# This will be replaced by Cookiecutter with the actual value
+print("post hook")
+print("_template =", "{{ cookiecutter._template }}")
+print("_checkout =", "{{ cookiecutter._checkout }}")
+
+# src = Path("file.json")
+# dst = Path("destination/file.json")
+# shutil.move(str(src), str(dst))
---
+++ foo/hooks/pre_gen_project.py
@@ -0,0 +1,11 @@
+# from pprint import pprint
+
+# This will be replaced by Cookiecutter with the actual value
+print("pre-hook")
+print("project_name =", "{{ cookiecutter.project_name }}")
+print("author =", "{{ cookiecutter.author }}")
+
+
+# src = Path("file.json")
+# dst = Path("destination/file.json")
+# shutil.move(str(src), str(dst))
